f57ad7fc5bd73dc096f4c4376ea773825339140a
Michael Granger <ged@FaerieMUD.org>
2011-04-12 12:55:49 -0700

    Fleshing out the README, adding scope-bleedover example

---
+++ experiments/erb_scope_bleed.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+
+require 'erb'
+
+TEMPLATE = <<END_TEMPLATE
+<% var = "foo" %>
+END_TEMPLATE
+
+var = 1
+template = ERB.new( TEMPLATE )
+
+puts "Before rendering, var = %p" % [ var ]
+output = template.result( binding() )
+puts "After rendering, var = %p" % [ var ]
+
+
