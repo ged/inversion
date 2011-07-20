f5de42843a3bb5a878fc187881e634e828c3f5cf
Michael Granger <ged@FaerieMUD.org>
2011-07-20 11:02:29 -0700

    Merging ANSIColor methods into the ColorLogFormatter, which was the only place the code was used.

--- lib/inversion/utils.rb
+++ lib/inversion/utils.rb
@@ -69,7 +69,63 @@ module Inversion
 	# A ANSI-colorized formatter for Logger instances.
 	# @private
 	class ColorLogFormatter < Logger::Formatter
-		extend Inversion::ANSIColorUtilities
+
+		# Set some ANSI escape code constants (Shamelessly stolen from Perl's
+		# Term::ANSIColor by Russ Allbery <rra@stanford.edu> and Zenin <zenin@best.com>
+		ANSI_ATTRIBUTES = {
+			'clear'      => 0,
+			'reset'      => 0,
+			'bold'       => 1,
+			'dark'       => 2,
+			'underline'  => 4,
+			'underscore' => 4,
+			'blink'      => 5,
+			'reverse'    => 7,
+			'concealed'  => 8,
+
+			'black'      => 30,   'on_black'   => 40,
+			'red'        => 31,   'on_red'     => 41,
+			'green'      => 32,   'on_green'   => 42,
+			'yellow'     => 33,   'on_yellow'  => 43,
+			'blue'       => 34,   'on_blue'    => 44,
+			'magenta'    => 35,   'on_magenta' => 45,
+			'cyan'       => 36,   'on_cyan'    => 46,
+			'white'      => 37,   'on_white'   => 47
+		}
+
+
+		### Create a string that contains the ANSI codes specified and return it
+		def self::ansi_code( *attributes )
+			attributes.flatten!
+			attributes.collect! {|at| at.to_s }
+			return '' unless /(?:vt10[03]|xterm(?:-color)?|linux|screen)/i =~ ENV['TERM']
+			attributes = ANSI_ATTRIBUTES.values_at( *attributes ).compact.join(';')
+
+			if attributes.empty?
+				return ''
+			else
+				return "\e[%sm" % attributes
+			end
+		end
+
+
+		### Colorize the given +string+ with the specified +attributes+ and
+		### return it, handling line-endings, color reset, etc.
+		def self::colorize( *args )
+			string = ''
+
+			if block_given?
+				string = yield
+			else
+				string = args.shift
+			end
+
+			ending = string[/(\s)$/] || ''
+			string = string.rstrip
+
+			return self.ansi_code( args.flatten ) + string + self.ansi_code( 'reset' ) + ending
+		end
+
 
 		# Color settings
 		LEVEL_FORMATS = {
@@ -116,6 +172,7 @@ module Inversion
 
 			return self.settings[ severity.downcase.to_sym ] % args
 		end
+
 	end # class LogFormatter
 
 
