d94085bee172549992e1e3ceea629236a316bfd1
Michael Granger <ged@FaerieMUD.org>
2011-08-15 10:53:51 -0700

    Bump patch version for package-fix release.

--- lib/inversion.rb
+++ lib/inversion.rb
@@ -26,7 +26,7 @@ module Inversion
 	require 'inversion/monkeypatches'
 
 	# Library version constant
-	VERSION = '0.0.1'
+	VERSION = '0.0.2'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
