598a024188274d1ba6c17cba82a6e10fcb96962e
Michael Granger <ged@FaerieMUD.org>
2011-09-23 16:27:21 -0700

    Added Inversion::Error and made all exception classes inherit from it.

--- lib/inversion/exceptions.rb
+++ lib/inversion/exceptions.rb
@@ -4,17 +4,20 @@
 #--
 module Inversion
 
+	# A generic Inversion exception class
+	class Error < ::RuntimeError; end
+
 	# An exception class raised from the Inversion::Template::Parser when
 	# a problem is encountered while parsing a template.
-	class ParseError < ::RuntimeError; end
+	class ParseError < Inversion::Error; end
 
 	# An exception class raised when a problem is detected in a template
 	# configuration option.
-	class OptionsError < ::RuntimeError; end
+	class OptionsError < Inversion::Error; end
 
 	# An exception class raised when a template includes itself, either
 	# directly or indirectly.
-	class StackError < ::RuntimeError; end
+	class StackError < Inversion::Error; end
 
 end # module Inversion
 
