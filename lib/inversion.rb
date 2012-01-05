c600327ce50a3ce3cd806a299fcb0c737ea886c4
Michael Granger <ged@FaerieMUD.org>
2012-01-05 08:04:37 -0800

    Bump the minor version, update history.

--- lib/inversion.rb
+++ lib/inversion.rb
@@ -26,7 +26,7 @@ module Inversion
 	require 'inversion/monkeypatches'
 
 	# Library version constant
-	VERSION = '0.4.0'
+	VERSION = '0.5.0'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
