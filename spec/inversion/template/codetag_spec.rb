9aa7e3f3c805c269eeaf25f4c18912fb2c169a65
Mahlon E. Smith <mahlon@martini.nu>
2017-07-19 10:25:36 -0700

    Provide a method for tags to explicitly inherit patterns from their parents, instead of replacing them.

--- spec/inversion/template/codetag_spec.rb
+++ spec/inversion/template/codetag_spec.rb
@@ -28,6 +28,46 @@ describe Inversion::Template::CodeTag do
 			expect( subclass.tag_patterns.first[1].call(:dummy, :king_dummy) ).to eq( :foo )
 		end
 
-	end
 
+		it "can explicitly declare pattern inheritence" do
+			parentclass = Class.new( Inversion::Template::CodeTag ) do
+				tag_pattern "$(ident)" do |tag, match|
+					:foo
+				end
+			end
+
+			subclass = Class.new( parentclass ) do
+				inherit_tag_patterns
+				tag_pattern "$(op) $(ident)" do |tag, match|
+					:bar
+				end
+			end
+
+			expect( subclass.tag_patterns.size ).to eq( 2 )
+			expect( subclass.tag_patterns.first[0] ).
+				to be_an_instance_of( Inversion::Template::CodeTag::TokenPattern )
+			expect( subclass.tag_patterns.last[0] ).
+				to be_an_instance_of( Inversion::Template::CodeTag::TokenPattern )
+			expect( subclass.tag_patterns.first[1].call(:dummy, :king_dummy) ).to eq( :foo )
+			expect( subclass.tag_patterns.last[1].call(:dummy, :king_dummy) ).to eq( :bar )
+		end
+
+
+		it "throws an error if trying to inherit patterns after they are declared" do
+			parentclass = Class.new( Inversion::Template::CodeTag ) do
+				tag_pattern "$(ident)" do |tag, match|
+					:foo
+				end
+			end
+
+			expect {
+				Class.new( parentclass ) do
+					tag_pattern "$(op) $(ident)" do |tag, match|
+						:bar
+					end
+					inherit_tag_patterns
+				end
+			}.to raise_exception( ScriptError, /patterns already exist/i )
+		end
+	end
 end
