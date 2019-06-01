5358a354da522b7e987544e5316f872364df611e
Michael Granger <ged@FaerieMUD.org>
2019-05-31 17:13:27 -0700

    Bump the minor version, update history

--- lib/inversion.rb
+++ lib/inversion.rb
@@ -26,7 +26,7 @@ module Inversion
 	warn ">>> Inversion requires Ruby 2.4.0 or later. <<<" if RUBY_VERSION < '2.4.0'
 
 	# Library version constant
-	VERSION = '1.1.1'
+	VERSION = '1.2.0'
 
 	# Version-control revision constant
 	REVISION = %q$Revision$
