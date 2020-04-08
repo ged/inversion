d1f1c2d0bd153d4061c0ab3827b33b5404844849
Mahlon E. Smith <mahlon@laika.com>
2020-04-08 15:02:57 -0700

    Unhoeify, update to v1.3.0.

---
+++ gem.deps.rb
@@ -0,0 +1,18 @@
+source 'https://rubygems.org/'
+
+gem 'highline', '~> 2.0'
+gem 'loggability', '~> 0.17'
+gem 'sysexits', '~> 1.2'
+gem 'trollop', '~> 2.9'
+
+group :development do
+    gem 'rack-test', '~> 1.1'
+	gem 'rake-deveiate', '~> 0.14'
+    gem 'rdoc', '~> 6.2'
+	gem 'rdoc-generator-fivefish', '~> 0.4'
+    gem 'rspec-wait', '~> 0.0'
+    gem 'simplecov', '~> 0.18'
+    gem 'sinatra', '~> 2.0'
+    gem 'tilt', '~> 2.0'
+end
+
